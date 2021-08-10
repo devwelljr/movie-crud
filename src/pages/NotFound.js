1ba215fd9d101a0881e2bfaa079ca2dfd02136a5
Wellington <junior.wel02@exemplo.br>
2021-08-10 15:11:53 -0300

    not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import './NotFound.css';
 
 class NotFound extends Component {
   render() {
-    return <div data-testid="404-error">Página não encontrada</div>;
+    return <div id="error" data-testid="404-error">Página não encontrada</div>;
   }
 }
 
